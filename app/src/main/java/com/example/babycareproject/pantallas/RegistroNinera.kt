72f1084fc5b132f4c400ecc9dbf490747d8ed4d9
Violeta Brusil <violet_9620@hotmail.com>
2020-01-21 22:52:49 -0600

    Creación pantalla registro niñera

---
+++ app/src/main/java/com/example/babycareproject/pantallas/RegistroNinera.kt
@@ -0,0 +1,12 @@
+package com.example.babycareproject.pantallas
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.babycareproject.R
+class RegistroNinera : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_ninera)
+    }
+}
